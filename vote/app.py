766156b356edefd1e4997a9f357a28535f5ca983
Sebb <sifa@SifaDesktop.fritz.box>
2020-02-11 08:06:43 +0100

    added question option

--- vote/app.py
+++ vote/app.py
@@ -5,9 +5,12 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+
+
+option_a = os.getenv('OPTION_A', "true")
+option_b = os.getenv('OPTION_B', "false")
 hostname = socket.gethostname()
+counter = 1
 
 app = Flask(__name__)
 
@@ -16,8 +19,28 @@ def get_redis():
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/", methods=['POST','GET'])
-def hello():
+@app.route("/", methods=['GET'])
+def index():
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+
+    vote = None
+
+    resp = make_response(render_template(
+        'index.html',
+        option_a=option_a,
+        option_b=option_b,
+        hostname=hostname,
+        vote=vote,
+    ))
+    resp.set_cookie('voter_id', voter_id)
+    return resp
+
+@app.route("/start", methods=['POST','GET'])
+def start():
+    count = counter
+    question = os.getenv('QUESTION_'+count, "Question")
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
@@ -31,7 +54,35 @@ def hello():
         redis.rpush('votes', data)
 
     resp = make_response(render_template(
-        'index.html',
+        'start.html',
+        question=question,
+        option_a=option_a,
+        option_b=option_b,
+        hostname=hostname,
+        vote=vote,
+    ))
+    resp.set_cookie('voter_id', voter_id)
+    return resp
+
+@app.route("/next", methods=['POST','GET'])
+def next():
+    count = counter + 1
+    question = os.getenv('QUESTION_'+count, "Question")
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+
+    vote = None
+
+    if request.method == 'POST':
+        redis = get_redis()
+        vote = request.form['vote']
+        data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        redis.rpush('votes', data)
+
+    resp = make_response(render_template(
+        'vote.html',
+        question=question,
         option_a=option_a,
         option_b=option_b,
         hostname=hostname,
@@ -42,4 +93,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
